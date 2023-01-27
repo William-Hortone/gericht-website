8cc1f5c7e76a83f9bf4a8ae64d09f34bc6c4aed9
William-Hortone <willhortone@gmail.com>
2023-01-27 23:00:31 +0800

    adding the header and about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt=" about spoon " className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+          accusantium eligendi itaque, voluptatem incidunt deleniti illum sint
+          officiis cupiditate consequatur.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife img" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt=" about spoon " className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+          accusantium eligendi itaque, voluptatem incidunt deleniti illum sint
+          officiis cupiditate consequatur.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1"> The Key to find Dining</h1>
+      <p className="p__opensans " style={{ margin: "2rem 0" }}>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        voluptate inventore earum, omnis amet labore accusamus voluptates
+        aperiam nam cum?
+      </p>
+      <button type="button" className="custom__button">
+        {" "}
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt=" welcome img" />
+    </div>
   </div>
 );
 
