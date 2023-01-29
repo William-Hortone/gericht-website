9655505843e77bc67b177a27ccd946b30cee52ed
William-Hortone <willhortone@gmail.com>
2023-01-30 02:23:32 +0800

    added the newsletter section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email" />
+      <button className="custom__button" type="button">
+        Subscribe
+      </button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
