99a0cb2ce375f198b96a509615ae121861535e69
William-Hortone <willhortone@gmail.com>
2023-01-29 14:03:11 +0800

    added gallery section

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
+// import { useRef } from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.css";
 
-import './Gallery.css';
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+  const gallery = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04,
+  ];
+  const scroll = (direction) => {
+    // console.log("hello");
+    const { current } = scrollRef;
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+      // console.log("left");
+    } else {
+      current.scrollLeft += 300;
+      // console.log("right");
+    }
+  };
 
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photos Gallery</h1>
+        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {gallery.map((image, index) => (
+            <div
+              className="app__gallery-images-card flex__center"
+              key={`gallery_image-${index + 1}`}
+            >
+              <img src={image} alt="gallery image" />
+
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Gallery;
